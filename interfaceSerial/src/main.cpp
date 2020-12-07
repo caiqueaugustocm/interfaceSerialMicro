64cfa532346db37a6d6a58c25164557d1f6ae763
Caique Moraes <bispo.caique@hotmail.com>
2020-12-07 16:13:52 -0300

    finished

--- interfaceSerial/src/main.cpp
+++ interfaceSerial/src/main.cpp
@@ -1,10 +1,50 @@
 #include <mbed.h>
 
+PwmOut led(LED1);
+
+Serial pc(USBTX, USBRX, 9600);
+
+char input;
+float ledIntensity;
+
+void getValue();
+void changeIntensity();
+
 int main() {
+  pc.attach(&getValue);
+}
 
-  // put your setup code here, to run once:
+void getValue()
+{
+  input = pc.getc();
+  changeIntensity();
+}
 
-  while(1) {
-    // put your main code here, to run repeatedly:
-  }
+void changeIntensity(){
+  switch(input)
+    {
+      case '+':
+        if (ledIntensity >= 1.0f)
+          ledIntensity = 1.0f;
+        else
+          ledIntensity += 0.5f;
+        break;
+      case '-':
+        if (ledIntensity <= 0.0f)
+          ledIntensity = 0.0f;
+        else
+          ledIntensity -= 0.5f;
+        break;
+      case '*':
+        ledIntensity = 1.0f;
+        break;
+      case '/':
+        ledIntensity = 0.0f;
+        break;
+      default:
+        pc.printf("Invalid character!\n");
+        break;
+    }
+    led.write(ledIntensity);
+    pc.printf("Led Intensity is: %.2f\n",ledIntensity);
 }
